9a51cfc207d4c407a673193f876c50d1c9a7ee04
David Illing <davidjames.illing@creditkarma.com>
2022-12-01 04:13:53 +0000

    formatting

--- src/test/scala/chapter02/isSortedSpec.scala
+++ src/test/scala/chapter02/isSortedSpec.scala
@@ -3,7 +3,7 @@ package chapter02
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.funsuite.AnyFunSuite
 
-class isSortedSpec extends AnyFunSuite with Matchers{
+class isSortedSpec extends AnyFunSuite with Matchers {
   test("isSorted") {
     isSorted(Array(1, 2, 3), _ > _) shouldBe true
     isSorted(Array(1, 2, 1), _ > _) shouldBe false
