1f94d568c2608c8a2f2dc00d66450dd0ff9d5fde
David Illing <davidjames.illing@creditkarma.com>
2023-01-24 19:19:48 -0500

    5.6

--- src/main/scala/chapter05/LazyList.scala
+++ src/main/scala/chapter05/LazyList.scala
@@ -4,36 +4,47 @@ enum LazyList[+A]:
   case Empty
   case Cons(h: () => A, t: () => LazyList[A])
 
+  import LazyList._
+
   def headOption: Option[A] = this match
-    case Empty => None
+    case Empty      => None
     case Cons(h, _) => Some(h())
 
   def toList: List[A] = this match
-    case Empty => List.empty
+    case Empty      => List.empty
     case Cons(h, t) => h() :: t().toList
 
   def take(n: Int): LazyList[A] = this match
-    case Empty => Empty
+    case Empty       => Empty
     case _ if n <= 0 => LazyList.empty
-    case Cons(h, t) => LazyList.cons(h(), t().take(n-1))
-  
+    case Cons(h, t)  => LazyList.cons(h(), t().take(n - 1))
+
   def drop(n: Int): LazyList[A] = this match
-    case Empty => Empty
+    case Empty       => Empty
     case l if n <= 0 => l
-    case Cons(_, t) => t().drop(n-1)
+    case Cons(_, t)  => t().drop(n - 1)
 
   def takeWhile(p: A => Boolean): LazyList[A] = this match
-    case Empty => Empty
+    case Empty                 => Empty
     case Cons(h, _) if !p(h()) => Empty
-    case Cons(h, t) => LazyList.cons(h(), t().takeWhile(p))
+    case Cons(h, t)            => LazyList.cons(h(), t().takeWhile(p))
 
   def foldRight[B](acc: => B)(f: (A, => B) => B): B =
     this match
       case Cons(h, t) => f(h(), t().foldRight(acc)(f))
-      case _ => acc
+      case _          => acc
 
   def exists(p: A => Boolean): Boolean =
     foldRight(false)((a, b) => p(a) || b)
+
+  def forAll(p: A => Boolean): Boolean =
+    foldRight(true)((a, b) => p(a) && b)
+
+  def takeWhile2(p: A => Boolean): LazyList[A] =
+    foldRight(LazyList.empty[A])((a, b) => if (p(a)) cons(a, b) else b)
+
+  def headOption2: Option[A] =
+    foldRight(Option.empty[A])((a, _) => Some(a))
 
 object LazyList:
   def cons[A](hd: => A, tl: => LazyList[A]): LazyList[A] =
--- src/test/scala/chapter05/chapter05spec.scala
+++ src/test/scala/chapter05/chapter05spec.scala
@@ -1,17 +1,19 @@
 package chapter05
 
-
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.prop.TableDrivenPropertyChecks
 import chapter05.LazyList
 
-class chapter05Spec extends AnyFreeSpec with Matchers with TableDrivenPropertyChecks {
+class chapter05Spec
+    extends AnyFreeSpec
+    with Matchers
+    with TableDrivenPropertyChecks {
   "5.1" in {
     val table = Table(
       ("list", "expected"),
-      (LazyList(1,2), List(1,2)),
-      (LazyList.empty, List.empty),
+      (LazyList(1, 2), List(1, 2)),
+      (LazyList.empty, List.empty)
     )
 
     forEvery(table) { (list, expected) =>
@@ -23,9 +25,9 @@ class chapter05Spec extends AnyFreeSpec with Matchers with TableDrivenPropertyCh
     "take" in {
       val table = Table(
         ("list", "expected"),
-        (LazyList(1,2), List(1,2)),
-        (LazyList(1,2,3,4), List(1,2,3)),
-        (LazyList.empty, List.empty),
+        (LazyList(1, 2), List(1, 2)),
+        (LazyList(1, 2, 3, 4), List(1, 2, 3)),
+        (LazyList.empty, List.empty)
       )
 
       forEvery(table) { (list, expected) =>
@@ -36,9 +38,9 @@ class chapter05Spec extends AnyFreeSpec with Matchers with TableDrivenPropertyCh
     "drop" in {
       val table = Table(
         ("list", "expected"),
-        (LazyList(1,2), List.empty),
-        (LazyList(1,2,3,4), List(4)),
-        (LazyList.empty, List.empty),
+        (LazyList(1, 2), List.empty),
+        (LazyList(1, 2, 3, 4), List(4)),
+        (LazyList.empty, List.empty)
       )
 
       forEvery(table) { (list, expected) =>
@@ -51,12 +53,51 @@ class chapter05Spec extends AnyFreeSpec with Matchers with TableDrivenPropertyCh
     val table = Table(
       ("list", "expected"),
       (LazyList(2), List(2)),
-      (LazyList(1,2,3,4), List(1,2)),
-      (LazyList.empty, List.empty),
+      (LazyList(1, 2, 3, 4), List(1, 2)),
+      (LazyList.empty, List.empty)
     )
 
     forEvery(table) { (list, expected) =>
       list.takeWhile(_ <= 2).toList shouldBe expected
     }
   }
+
+  "5.4" in {
+    val table = Table(
+      ("list", "expected"),
+      (LazyList(1, 2, 3), false),
+      (LazyList(2, 4), true),
+      (LazyList.empty, true)
+    )
+
+    forEvery(table) { (list, expected) =>
+      list.forAll(_ % 2 == 0) shouldBe expected
+    }
+  }
+
+  "5.5" in {
+    val table = Table(
+      ("list", "expected"),
+      (LazyList(2), List(2)),
+      (LazyList(1, 2, 3, 4), List(1, 2)),
+      (LazyList.empty, List.empty)
+    )
+
+    forEvery(table) { (list, expected) =>
+      list.takeWhile2(_ <= 2).toList shouldBe expected
+    }
+  }
+
+  "5.6" in {
+    val table = Table(
+      ("list", "expected"),
+      (LazyList(1, 2), Some(1)),
+      (LazyList.empty, None)
+    )
+
+    forEvery(table) { (list, expected) =>
+      list.headOption2 shouldBe expected
+    }
+  }
+
 }
